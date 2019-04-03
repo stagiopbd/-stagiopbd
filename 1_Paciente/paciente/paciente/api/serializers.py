a3e2156f90da5c572b520d7f721a6715a46dd4c6
gustavogomides <gustavo.gomides7@gmail.com>
2019-04-02 22:39:20 -0300

    Se aplicado, este commit implementará validação condicional requisição

--- 1_Paciente/paciente/paciente/api/serializers.py
+++ 1_Paciente/paciente/paciente/api/serializers.py
@@ -9,6 +9,18 @@ class PacienteSerializer(serializers.ModelSerializer):
 
 
 class QuadroClinicoSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = QuadroClinico
         fields = '__all__'
+
+    def check_exists(self, data, father_field, children_field):
+        if data.get(father_field) and not data.get(children_field):
+            raise serializers.ValidationError(
+                "Field {} is required".format(children_field))
+
+    def validate(self, data):
+        self.check_exists(data, 'fuma', 'fuma_frequencia')
+        self.check_exists(data, 'bebe', 'bebe_frequencia')
+        self.check_exists(data, 'pratica_atividade', 'pratica_frequencia')
+        return data
