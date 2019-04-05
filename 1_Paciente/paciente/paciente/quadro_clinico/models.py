9614ed31554fc873e706ffc900f6ccf3af369d1e
gustavogomides <gustavo.gomides7@gmail.com>
2019-04-05 07:06:16 -0300

    Se aplicado, este commit implementará frequência como combobox

--- 1_Paciente/paciente/paciente/quadro_clinico/models.py
+++ 1_Paciente/paciente/paciente/quadro_clinico/models.py
@@ -3,17 +3,26 @@ from paciente.models import Paciente
 
 
 class QuadroClinico(models.Model):
+    FREQUENCIAS = (
+        ("nunca", "nunca"),
+        ("raramente", "raramente"),
+        ("normalmente", "normalmente"),
+        ("frequentemente", "frequentemente"),
+        ("sempre", "sempre")
+    )
     paciente = models.OneToOneField(Paciente, on_delete=models.CASCADE)
     peso = models.FloatField()
     altura = models.FloatField()
     imc = models.CharField(max_length=120, blank=True, null=True)
     fuma = models.BooleanField()
-    fuma_frequencia = models.CharField(max_length=120, blank=True, null=True)
+    fuma_frequencia = models.CharField(
+        max_length=120, blank=True, null=True, choices=FREQUENCIAS)
     bebe = models.BooleanField()
-    bebe_frequencia = models.CharField(max_length=120, blank=True, null=True)
+    bebe_frequencia = models.CharField(
+        max_length=120, blank=True, null=True, choices=FREQUENCIAS)
     pratica_atividade = models.BooleanField()
     pratica_frequencia = models.CharField(
-        max_length=120, blank=True, null=True)
+        max_length=120, blank=True, null=True, choices=FREQUENCIAS)
 
     class Meta:
         db_table = "quadro_clinico"
--- 1_Paciente/paciente/paciente/quadro_clinico/utils.py
+++ 1_Paciente/paciente/paciente/quadro_clinico/utils.py
@@ -4,16 +4,12 @@ import numpy as np
 def calc_imc(peso, altura):
     imc = np.divide(peso, np.power(altura, 2))
 
-    if imc < 16:
-        imc_classe = "Magreza grau III"
-    elif imc < 17:
-        imc_classe = "Magreza grau II"
-    elif imc < 18.5:
-        imc_classe = "Magreza grau II"
+    if imc < 18.5:
+        imc_classe = "Abaixo do peso"
     elif imc < 25:
-        imc_classe = "Saudável"
+        imc_classe = "Peso ideal (parabéns)"
     elif imc < 30:
-        imc_classe = "Sobrepeso"
+        imc_classe = "Levemente acima do peso"
     elif imc < 35:
         imc_classe = "Obesidade Grau I"
     elif imc < 40:
