867976b9adb28fd7c790cad3ef7b1b157d47608c
lunnakfouri <lunna.ags@gmail.com>
2019-04-05 23:44:55 -0300

    Exibicao de Lista de Hospitais

--- 3_Hospital/api/controllers/hospital-controller.js
+++ 3_Hospital/api/controllers/hospital-controller.js
@@ -1,5 +1,6 @@
 'use strict';
 var hospital = require('../models/hospital-model')
+//http://docs.sequelizejs.com/manual/models-usage.html
 
 exports.post = (req, res, next) => {
     var me = req.body
@@ -10,9 +11,18 @@ exports.post = (req, res, next) => {
         telefone:  me.inputAddress
     })
     //console.log(req.body)
-    res.send('seja bem vindo. ' + req.body.inputPhone) 
+    //res.send('seja bem vindo. ' + req.body.inputPhone) 
+    res.redirect('/Hospital')
 }; 
 
 exports.get = (req, res, next) => {
-    res.render('hospital', { title: 'Cadastro de Hospital' })
+    hospital.findAll({order : [['Nome', 'ASC']]}).then(function(hsp){
+        //console.log(hsp)
+        res.render('hospital-lista', { title: 'Lista de Hospitais', hsp: hsp })
+    })
+    //res.render('hospital', { title: 'Cadastro de Hospital' })
+};
+
+exports.cadastro = (req, res, next) => {
+    res.render('hospital-cadastro', { title: 'Cadastro de Hospital' })
 };
--- 3_Hospital/api/routes/hospital-route.js
+++ 3_Hospital/api/routes/hospital-route.js
@@ -6,6 +6,7 @@ const controller = require('../controllers/hospital-controller')
 
 module.exports = function(app) {
     router.get('/', controller.get);
+    router.get('/Cadastro', controller.cadastro);
     router.post('/', controller.post);
     //router.put('/:id', controller.put);
 	//router.delete('/', controller.delete);
