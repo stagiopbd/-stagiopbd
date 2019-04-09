7775f06f56980c167bb4e817e1699af64f974e5b
gustavogomides <gustavo.gomides7@gmail.com>
2019-04-08 21:41:52 -0300

    Implementação interface paciente tem alergia

--- 1_Paciente/paciente/paciente/api/urls.py
+++ 1_Paciente/paciente/paciente/api/urls.py
@@ -4,12 +4,13 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from paciente.views import PacienteViewSet, index_paciente, index
-from alergia.views import AlergiaViewSet, index_alergia
+from alergia.views import AlergiaViewSet, PacienteTemAlergiaViewSet, index_alergia
 from quadro_clinico.views import QuadroClinicoViewSet, index_quadro_clinico
 
 router = SimpleRouter(trailing_slash=False)
 router.register(r'paciente', PacienteViewSet)
 router.register(r'alergia', AlergiaViewSet)
+router.register(r'paciente-alergia', PacienteTemAlergiaViewSet)
 router.register(r'quadro_clinico', QuadroClinicoViewSet)
 
 urlpatterns = [
